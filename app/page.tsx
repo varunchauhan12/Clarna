e7f24d72f1f23aa56aae217d48462dc9996e7cf3
varun <varunchauhan_mc24b06_058@dtu.ac.in>
2025-07-12 02:13:40 +0530

    setup trpc and server side prefetching using advanced rendering techniques

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
-import {prisma} from "@/lib/db";
+import {getQueryClient , trpc } from "@/trpc/server";
+import {dehydrate, HydrationBoundary} from "@tanstack/react-query";
+import Client from "@/app/Client";
+import {Suspense} from "react";
+
 
 export default async function Home() {
-
-    const users = await prisma.user.findMany();
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text : "hello from the prefetch"}))
   return (
-    <div>
-        {JSON.stringify(users , null , 2)}
-
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Client />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
