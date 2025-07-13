81c57d655e69622a3afe1c3502050c671fff4a17
varun <varunchauhan_mc24b06_058@dtu.ac.in>
2025-07-13 23:13:42 +0530

    set up background jobs by inngest

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import {getQueryClient , trpc } from "@/trpc/server";
-import {dehydrate, HydrationBoundary} from "@tanstack/react-query";
-import Client from "@/app/Client";
-import {Suspense} from "react";
+'use client';
+
+import {Button} from "@/components/ui/button";
+import {useTRPC} from "@/trpc/client";
+import {useMutation} from "@tanstack/react-query";
 
 
-export default async function Home() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({text : "hello from the prefetch"}))
+export default function Home() {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<p>Loading...</p>}>
-      <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className={'p-4 mx-auto'}>
+      <Button onClick={()=>invoke.mutate({text : "hello world"})}>Invoke inngest</Button>
+    </div>
   );
 }
